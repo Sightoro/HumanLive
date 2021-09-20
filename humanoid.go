738ca0d0aca38d04b1be108ffe63d941055ccf72
Sightoro <yuralon.skiy@mail.ru>
2021-09-20 17:07:04 +0300

    nothingInteresting

--- humanoid.go
+++ humanoid.go
@@ -3,17 +3,18 @@ package main
 import (
 	"fmt"
 	"math"
+	"os"
 )
 
 type Persone struct {
-	Name string
-	Age int
-	Health int
-	Wallet int
-	IceBox int
+	Name    string
+	Age     int
+	Health  int
+	Wallet  int
+	IceBox  int
 	WorkDay int
-	Cost int
-	Energy int
+	Cost    int
+	Energy  int
 }
 
 type PersoneDo interface {
@@ -28,8 +29,8 @@ func (chel *Persone) Olding() {
 	chel.Age += 1
 }
 
-func (chel *Persone) Eat(){
-	if chel.Health > 100{
+func (chel *Persone) Eat() {
+	if chel.Health > 100 {
 		chel.Health = 100
 	}
 	if chel.IceBox >= chel.Energy {
@@ -41,12 +42,12 @@ func (chel *Persone) Sleep() {
 	chel.Health += 40
 }
 
-func (chel *Persone) Work(){
+func (chel *Persone) Work() {
 	chel.Health -= 30
 	chel.Wallet += chel.WorkDay
 }
 
-func (chel *Persone)  Pay(){
+func (chel *Persone) Pay() {
 	if chel.Wallet >= chel.Cost {
 		chel.Wallet -= chel.Cost
 		chel.IceBox += 4
@@ -54,11 +55,11 @@ func (chel *Persone)  Pay(){
 	}
 }
 
-func do(p PersoneDo,a *Persone) {
+func do(p PersoneDo, a *Persone) {
 	damage := 0.0
 	days := 0
 	i := float64(a.Age)
-	for a.Health > 0{
+	for a.Health > 0 {
 		days += 1
 		if days > 360 {
 			p.Olding()
@@ -66,44 +67,47 @@ func do(p PersoneDo,a *Persone) {
 			i += 1
 		}
 		//		p.Olding()
-		damage = math.Pow(0.04, -i / 100)
+		damage = math.Pow(0.04, -i/100)
 		a.Health -= int(damage)
-		if a.Health <= 0{
+		if a.Health <= 0 {
 			break
 		}
 		p.Eat()
-		if a.Health > 100{
+		if a.Health > 100 {
 			a.Health = 100
 		}
 		p.Work()
-		if a.Health <= 0{
+		if a.Health <= 0 {
 			break
 		}
 		p.Pay()
-		if a.Health <= 0{
+		if a.Health <= 0 {
 			break
 		}
 		p.Eat()
-		if a.Health > 100{
+		if a.Health > 100 {
 			a.Health = 100
 		}
 		p.Sleep()
-		if a.Health > 100{
+		if a.Health > 100 {
 			a.Health = 100
 		}
-		fmt.Println()
 	}
 }
 
-func main()  {
+func main() {
+	var name string
+	fmt.Print("inter name")
+	fmt.Fscan(os.Stdin, &name)
 	persone := &Persone{
 		Health:  100,
-		IceBox: 4,
-		Age: 33,
-		Wallet: 20000,
-		WorkDay: 1000,
-		Cost: 2000,
-		Energy: 2,
+		IceBox:  4,
+		Age:     33,
+		Wallet:  20000,
+		WorkDay: 300000,
+		Cost:    2000,
+		Energy:  2,
+		Name:    name,
 	}
 	do(persone, persone)
 	fmt.Println(persone.Age)
